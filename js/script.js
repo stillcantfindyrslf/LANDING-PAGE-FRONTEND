9d90a5f64da7d631432896d6d2e36a79394fb218
fkpe <ilasavic511@gmail.com>
2024-10-07 13:50:38 +0300

    L-12: - add discount time

--- js/script.js
+++ js/script.js
@@ -12,3 +12,30 @@ document.getElementById('additional-service__title').addEventListener('click', f
         }
     })
 })
+
+function startTime(hours, minutes, seconds) {
+    const timeElement = document.getElementById('orderDiscountTime');
+    let remainingTime = new Date();
+
+    remainingTime.setHours(hours);
+    remainingTime.setMinutes(minutes);
+    remainingTime.setSeconds(seconds);
+
+    const interval = setInterval(() => {
+        remainingTime.setSeconds(remainingTime.getSeconds() - 1);
+        const hoursLeft = remainingTime.getHours();
+        const minutesLeft = remainingTime.getMinutes();
+        const secondsLeft = remainingTime.getSeconds();
+        if(hoursLeft === 0 && minutesLeft === 0 && secondsLeft === 0){
+            clearInterval(interval);
+            timeElement.textContent = '00:00:00';
+            return;
+        }
+        timeElement.textContent =
+            String(hoursLeft).padStart(2, '0') + ':' +
+            String(minutesLeft).padStart(2, '0') + ':' +
+            String(secondsLeft).padStart(2, '0');
+    }, 1000);
+}
+
+startTime(5, 37, 25);
